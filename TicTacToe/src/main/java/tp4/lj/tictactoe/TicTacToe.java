9f7de20d1c5de374f06ce560533f5c4ed0797e75
luisestevam <luiseduardoestevam@gmail.com>
2024-06-16 17:46:30 +0100

    correção da verificação se há peças no mesmo lugar

--- TicTacToe/src/main/java/tp4/lj/tictactoe/TicTacToe.java
+++ TicTacToe/src/main/java/tp4/lj/tictactoe/TicTacToe.java
@@ -11,7 +11,7 @@ class Pecas {
     char tipo;
     static int nJogadas = 0;
 
-    Pecas(int x, int y) {
+    Pecas(int y, int x) {
         this.x = x;
         this.y = y;
         if (nJogadas % 2 == 0) {
@@ -45,6 +45,10 @@ class Tabuleiro {
         for (Pecas peca : todasPecas) {
             localizacaoPecas[peca.y][peca.x] = peca.tipo;
         }
+        imprimirTabuleiro();
+    }
+
+    void imprimirTabuleiro() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (j != 2) {
@@ -58,55 +62,54 @@ class Tabuleiro {
     }
 }
 
-public class TicTacToe{
-    
+public class TicTacToe {
     static Scanner scan = new Scanner(System.in);
-    
-    static void jogar(Tabuleiro tabuleiro, ArrayList<Pecas> todasPecas){
+
+    static void jogar(Tabuleiro tabuleiro, ArrayList<Pecas> todasPecas) {
         int x;
         int y;
-        
+
         System.out.println("");
-        if(Pecas.nJogadas % 2 != 0){
-            System.out.println("Agora e a vez do jogador2 (O)");
-        }else{
+        if (Pecas.nJogadas % 2 == 0) {
             System.out.println("Agora e a vez do jogador1 (X)");
+        } else {
+            System.out.println("Agora e a vez do jogador2 (O)");
         }
-        System.out.println("Posicao x:");
-        x = scan.nextInt() - 1;
-        if (x < 0 || x > 2){
-            System.out.println("Valor invalido a posição x deve ser entre 1 e 3:");
-            do{
-                x = scan.nextInt() - 1;
-            }while(x < 0 || x > 2);
+
+        do {
+            System.out.println("Posicao x (1 a 3):");
+            x = scan.nextInt() - 1;
+            if (x < 0 || x > 2) {
+                System.out.println("Valor invalido a posição x deve ser entre 1 e 3");
+            }
+        } while (x < 0 || x > 2);
+
+        do {
+            System.out.println("Posicao y (1 a 3):");
+            y = scan.nextInt() - 1;
+            if (y < 0 || y > 2) {
+                System.out.println("Valor invalido a posicao y deve ser entre 1 e 3");
+            }
+        } while (y < 0 || y > 2);
+
+        // Verifica se a posição já está ocupada por 'X' ou 'O'
+        if (tabuleiro.localizacaoPecas[y][x] == 'X' || tabuleiro.localizacaoPecas[y][x] == 'O') {
+            System.out.println("Posicao ja ocupada, tente novamente.");
+            jogar(tabuleiro, todasPecas);
+            return;
         }
-        System.out.println("Posicao y:");
-        y = scan.nextInt() - 1;
-                if (y < 0 || y > 2){
-            System.out.println("Valor invalido a posição y deve ser entre 1 e 3:");
-            do{
-                y = scan.nextInt() - 1;
-            }while(y < 0 || y > 2);
-        }
-        
-        if (tabuleiro.localizacaoPecas[x][y] != ' ') {
-           System.out.println("Posição já ocupada, tente novamente.");
-           jogar(tabuleiro, todasPecas);
-           return;
-        }
-        
-        Pecas peca = new Pecas(x, y);
+
+        Pecas peca = new Pecas(y, x);
         todasPecas.add(peca);
         tabuleiro.atualizarTabuleiro(todasPecas);
-        
     }
-    
+
     public static void main(String[] args) {
-        int rodar = 1;
         Tabuleiro tabuleiro = new Tabuleiro();
         ArrayList<Pecas> todasPecas = new ArrayList<>();
         tabuleiro.escreverRegras();
-        while(rodar == 1){
+        tabuleiro.imprimirTabuleiro(); // Imprime o tabuleiro inicial
+        while (true) {
             jogar(tabuleiro, todasPecas);
         }
     }
